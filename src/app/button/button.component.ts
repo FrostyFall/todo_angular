b6de2148caf8a105db0446b3059f48c4c4a44dbb
Artem Yakovlev <thenightfall808@icloud.com>
2022-08-18 13:24:37 +0300

    add-button-component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'custom-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input()
+  type: string | null = null;
+
+  @Input()
+  size: string | null = null;
+
+  constructor() {}
+}
